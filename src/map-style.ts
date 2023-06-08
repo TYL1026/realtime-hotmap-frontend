a03f69e7a9e70cd42ac2f2f1be8ddca7c3b47835
RuiWang98 <benjaminwr98@gmail.com>
2023-06-07 21:25:00 -0400

    Added sample data for testing

--- src/map-style.ts
+++ src/map-style.ts
@@ -1,6 +1,6 @@
 import type {HeatmapLayer} from 'react-map-gl';
 
-const MAX_ZOOM_LEVEL = 9;
+const MAX_ZOOM_LEVEL = 14;
 
 export const heatmapLayer: HeatmapLayer = {
   id: 'heatmap',
@@ -8,10 +8,10 @@ export const heatmapLayer: HeatmapLayer = {
   type: 'heatmap',
   paint: {
     // Increase the heatmap weight based on frequency and property magnitude
-    'heatmap-weight': ['interpolate', ['linear'], ['get', 'mag'], 0, 0, 6, 1],
+    'heatmap-weight': ['interpolate', ['linear'], ['get', 'density'], 0, 0, 1, 1],
     // Increase the heatmap color weight weight by zoom level
     // heatmap-intensity is a multiplier on top of heatmap-weight
-    'heatmap-intensity': ['interpolate', ['linear'], ['zoom'], 0, 1, MAX_ZOOM_LEVEL, 3],
+    'heatmap-intensity': ['interpolate', ['linear'], ['zoom'], 0, 1, MAX_ZOOM_LEVEL, 5],
     // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
     // Begin color ramp at 0-stop with a 0-transparancy color
     // to create a blur-like effect.
@@ -33,8 +33,8 @@ export const heatmapLayer: HeatmapLayer = {
       'rgb(255,201,101)'
     ],
     // Adjust the heatmap radius by zoom level
-    'heatmap-radius': ['interpolate', ['linear'], ['zoom'], 0, 2, MAX_ZOOM_LEVEL, 20],
+    'heatmap-radius': ['interpolate', ['linear'], ['zoom'], 0, 2, MAX_ZOOM_LEVEL, 10],
     // Transition from heatmap to circle layer by zoom level
-    'heatmap-opacity': ['interpolate', ['linear'], ['zoom'], 7, 1, 9, 0]
+    'heatmap-opacity': ['interpolate', ['linear'], ['zoom'], 12, 1, 14, 0]
   }
 };
--- src/routers/UberMap.tsx
+++ src/routers/UberMap.tsx
@@ -1,34 +1,41 @@
-import { Box, Container } from '@mui/material'
-import React from 'react'
-import MapGL, { Layer, Map, Source} from 'react-map-gl'
+import { Box, Container } from "@mui/material";
+import React, { useState } from "react";
+import MapGL, { Layer, Map, Source } from "react-map-gl";
+import { heatmapLayer } from "../map-style";
+import * as dataJson from "../data/data.json";
+import { GeoJsonData } from "../utils/GeoJsonData";
 
-const MAPBOX_TOKEN = 'pk.eyJ1IjoiYmVuamE5OCIsImEiOiJjbGlpYzZuOHUxdHV6M2dwN2M5bXNsZTFrIn0.9aQuvhbH6EifAfRcMX-dug';
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiYmVuamE5OCIsImEiOiJjbGlpYzZuOHUxdHV6M2dwN2M5bXNsZTFrIn0.9aQuvhbH6EifAfRcMX-dug";
+
+
+
+
+const data:any =  dataJson
+  
 
 const UberMap = () => {
-  return (
-    <Box mx={0} my={0} >
-        <MapGL
-            initialViewState={{
-            latitude: 40.71,
-            longitude: -74,
-            zoom: 12,
-            }}
-            style={{width: "100vw", height: "100vh"}}
 
-            mapStyle="mapbox://styles/mapbox/dark-v9"
-            mapboxAccessToken={MAPBOX_TOKEN}
-        >
-            {/* {data && (
+  return (
+    <Box mx={0} my={0}>
+      <MapGL
+        initialViewState={{
+          latitude: 40.71,
+          longitude: -74,
+          zoom: 12,
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxAccessToken={MAPBOX_TOKEN}
+      >
+        {data && (
             <Source type="geojson" data={data}>
                 <Layer {...heatmapLayer} />
             </Source>
-            )} */}
-        </MapGL>
-        
-
-        
+            )}
+      </MapGL>
     </Box>
-  )
-}
+  );
+};
 
-export default UberMap
+export default UberMap;
